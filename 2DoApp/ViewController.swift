4474dde573a9affb2cb7b3fe7a14040693c2b5e3
azimgunes <azimgunesofficial@gmail.com>
2024-02-12 13:44:18 +0300

    Functions were written.

--- 2DoApp/ViewController.swift
+++ 2DoApp/ViewController.swift
@@ -7,18 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    private var models = [TodoListItems]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "2Do App"
+        view.addSubview(tableView)
+        getAllItems()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .white
+        tableView.frame = view.bounds
+    
+        DispatchQueue.main.async { [self] in
+            tableView.reloadData()
+        }
     }
-
+    
+    
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        didTapForAdd()
+    }
+    
+    @objc func didTapForAdd(){
+        let alert = UIAlertController(title: "New Goal", message: "Write New Goal", preferredStyle: UIAlertController.Style.alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "OKEY", style: UIAlertAction.Style.cancel, handler: { [weak self]_ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else {
+                return
+            }
+            self?.createItem(name: text)
+            
+        }))
+        present(alert, animated: true)
+    }
     
     @objc func getAllItems (){
-        let items = try? context.fetch(TodoListItems.fetchRequest())
+        models = try! context.fetch(TodoListItems.fetchRequest())
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
         
     }
 
@@ -29,19 +66,62 @@ class ViewController: UIViewController {
         newItem.createdAt = Date()
         
         try? context.save()
+        
+        getAllItems()
     }
     
     @objc func deleteItem(item: TodoListItems){
         context.delete(item)
-        
+        getAllItems()
         try? context.save()
     }
     
     @objc func updateItem(item: TodoListItems, newName: String) {
         item.name = newName
-        
+        getAllItems()
         try? context.save()
         
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: [indexPath.row])
+        cell.textLabel?.text = model.name
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let item = models[indexPath.row]
+        
+        let edit = UIAlertController(title: "Edit Goal", message: "What's the change?", preferredStyle: UIAlertController.Style.alert)
+        edit.addAction(UIAlertAction(title: "CANCEL", style: UIAlertAction.Style.cancel, handler: nil))
+        edit.addAction(UIAlertAction(title: "EDIT", style: UIAlertAction.Style.default, handler: { _ in
+            let alert = UIAlertController(title: "New Goal", message: "Write New Goal", preferredStyle: UIAlertController.Style.alert)
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: UIAlertAction.Style.cancel, handler: { [weak self]_ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else {
+                    return
+                }
+                self?.updateItem(item: item, newName: newName)
+                
+            }))
+            self.present(alert, animated: true)
+        }))
+        edit.addAction(UIAlertAction(title: "DELETE", style: UIAlertAction.Style.destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+
+        present(edit, animated: true)
+    }
+    
 }
 
